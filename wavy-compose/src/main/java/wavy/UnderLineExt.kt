08f93d3dc45eaa65df50d4ca3dbdea0f158bd3b4
ansehoon1999 <ansehoon1999@gmail.com>
2023-07-23 10:47:05 +0900

    Wavy 네이밍 변경

--- wavy-compose/src/main/java/wavy/UnderLineExt.kt
+++ wavy-compose/src/main/java/wavy/UnderLineExt.kt
@@ -4,7 +4,6 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.ContentDrawScope
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 
 fun drawAccentLine(
     contentDrawScope: ContentDrawScope,
