752601bcde82e56d8d25d7cd096386a0ea528c0d
안세훈 <ansehoon1999@gmail.com>
2023-08-31 23:06:04 +0900

    chore: add README.md compose document & change naming (EditText -> Wavy)

--- wavy-sample/src/main/java/wavy/sample/MainActivity.kt
+++ wavy-sample/src/main/java/wavy/sample/MainActivity.kt
@@ -3,14 +3,14 @@ package wavy.sample
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import wavy.EditText
+import wavy.Wavy
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editText = findViewById<EditText>(R.id.edit_text)
+        val editText = findViewById<Wavy>(R.id.edit_text)
 
         findViewById<Button>(R.id.button).apply {
             setOnClickListener {
